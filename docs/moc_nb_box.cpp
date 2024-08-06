e521e0ecb66d4c7f3ff6684cf759ee368fbd37aa
lisr-pcx <lisr.pcx@gmail.com>
2024-08-06 17:29:09 +0100

    updated live demo

--- docs/moc_nb_box.cpp
+++ docs/moc_nb_box.cpp
@@ -41,23 +41,41 @@ static constexpr auto qt_meta_stringdata_CLASSNB_BoxENDCLASS = QtMocHelpers::str
     "NB_Box",
     "NewStatus",
     "",
-    "index",
+    "NB_Box*",
+    "ptr_box",
+    "RotateBoxes",
+    "angle",
+    "intensity",
     "mousePressEvent",
     "QGraphicsSceneMouseEvent*",
     "event",
-    "mouseReleaseEvent"
+    "mouseReleaseEvent",
+    "BoxStatus",
+    "BLUEBERRY",
+    "PLAYER1",
+    "PLAYER2",
+    "NUMBER_OF_STATES"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSNB_BoxENDCLASS_t {
-    uint offsetsAndSizes[16];
+    uint offsetsAndSizes[34];
     char stringdata0[7];
     char stringdata1[10];
     char stringdata2[1];
-    char stringdata3[6];
-    char stringdata4[16];
-    char stringdata5[26];
+    char stringdata3[8];
+    char stringdata4[8];
+    char stringdata5[12];
     char stringdata6[6];
-    char stringdata7[18];
+    char stringdata7[10];
+    char stringdata8[16];
+    char stringdata9[26];
+    char stringdata10[6];
+    char stringdata11[18];
+    char stringdata12[10];
+    char stringdata13[10];
+    char stringdata14[8];
+    char stringdata15[8];
+    char stringdata16[17];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSNB_BoxENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -66,20 +84,38 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSNB_BoxENDCLASS_t qt_meta_string
         QT_MOC_LITERAL(0, 6),  // "NB_Box"
         QT_MOC_LITERAL(7, 9),  // "NewStatus"
         QT_MOC_LITERAL(17, 0),  // ""
-        QT_MOC_LITERAL(18, 5),  // "index"
-        QT_MOC_LITERAL(24, 15),  // "mousePressEvent"
-        QT_MOC_LITERAL(40, 25),  // "QGraphicsSceneMouseEvent*"
-        QT_MOC_LITERAL(66, 5),  // "event"
-        QT_MOC_LITERAL(72, 17)   // "mouseReleaseEvent"
+        QT_MOC_LITERAL(18, 7),  // "NB_Box*"
+        QT_MOC_LITERAL(26, 7),  // "ptr_box"
+        QT_MOC_LITERAL(34, 11),  // "RotateBoxes"
+        QT_MOC_LITERAL(46, 5),  // "angle"
+        QT_MOC_LITERAL(52, 9),  // "intensity"
+        QT_MOC_LITERAL(62, 15),  // "mousePressEvent"
+        QT_MOC_LITERAL(78, 25),  // "QGraphicsSceneMouseEvent*"
+        QT_MOC_LITERAL(104, 5),  // "event"
+        QT_MOC_LITERAL(110, 17),  // "mouseReleaseEvent"
+        QT_MOC_LITERAL(128, 9),  // "BoxStatus"
+        QT_MOC_LITERAL(138, 9),  // "BLUEBERRY"
+        QT_MOC_LITERAL(148, 7),  // "PLAYER1"
+        QT_MOC_LITERAL(156, 7),  // "PLAYER2"
+        QT_MOC_LITERAL(164, 16)   // "NUMBER_OF_STATES"
     },
     "NB_Box",
     "NewStatus",
     "",
-    "index",
+    "NB_Box*",
+    "ptr_box",
+    "RotateBoxes",
+    "angle",
+    "intensity",
     "mousePressEvent",
     "QGraphicsSceneMouseEvent*",
     "event",
-    "mouseReleaseEvent"
+    "mouseReleaseEvent",
+    "BoxStatus",
+    "BLUEBERRY",
+    "PLAYER1",
+    "PLAYER2",
+    "NUMBER_OF_STATES"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -91,26 +127,37 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSNB_BoxENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
-       0,    0, // enums/sets
+       1,   54, // enums/sets
        0,    0, // constructors
        0,       // flags
-       1,       // signalCount
+       2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    1,   32,    2, 0x06,    1 /* Public */,
+       1,    1,   38,    2, 0x06,    1 /* Public */,
+       5,    3,   41,    2, 0x06,    3 /* Public */,
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       4,    1,   35,    2, 0x0a,    3 /* Public */,
-       7,    1,   38,    2, 0x0a,    5 /* Public */,
+       8,    1,   48,    2, 0x0a,    7 /* Public */,
+      11,    1,   51,    2, 0x0a,    9 /* Public */,
 
  // signals: parameters
-    QMetaType::Void, QMetaType::UInt,    3,
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, 0x80000000 | 3, QMetaType::Int, QMetaType::Int,    4,    6,    7,
 
  // slots: parameters
-    QMetaType::Void, 0x80000000 | 5,    6,
-    QMetaType::Void, 0x80000000 | 5,    6,
+    QMetaType::Void, 0x80000000 | 9,   10,
+    QMetaType::Void, 0x80000000 | 9,   10,
+
+ // enums: name, alias, flags, count, data
+      12,   12, 0x0,    4,   59,
+
+ // enum data: key, value
+      13, uint(NB_Box::BLUEBERRY),
+      14, uint(NB_Box::PLAYER1),
+      15, uint(NB_Box::PLAYER2),
+      16, uint(NB_Box::NUMBER_OF_STATES),
 
        0        // eod
 };
@@ -126,7 +173,12 @@ Q_CONSTINIT const QMetaObject NB_Box::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<NB_Box, std::true_type>,
         // method 'NewStatus'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<unsigned int, std::false_type>,
+        QtPrivate::TypeAndForceComplete<NB_Box *, std::false_type>,
+        // method 'RotateBoxes'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<NB_Box *, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
         // method 'mousePressEvent'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         QtPrivate::TypeAndForceComplete<QGraphicsSceneMouseEvent *, std::false_type>,
@@ -143,17 +195,43 @@ void NB_Box::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
         auto *_t = static_cast<NB_Box *>(_o);
         (void)_t;
         switch (_id) {
-        case 0: _t->NewStatus((*reinterpret_cast< std::add_pointer_t<uint>>(_a[1]))); break;
-        case 1: _t->mousePressEvent((*reinterpret_cast< std::add_pointer_t<QGraphicsSceneMouseEvent*>>(_a[1]))); break;
-        case 2: _t->mouseReleaseEvent((*reinterpret_cast< std::add_pointer_t<QGraphicsSceneMouseEvent*>>(_a[1]))); break;
+        case 0: _t->NewStatus((*reinterpret_cast< std::add_pointer_t<NB_Box*>>(_a[1]))); break;
+        case 1: _t->RotateBoxes((*reinterpret_cast< std::add_pointer_t<NB_Box*>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[3]))); break;
+        case 2: _t->mousePressEvent((*reinterpret_cast< std::add_pointer_t<QGraphicsSceneMouseEvent*>>(_a[1]))); break;
+        case 3: _t->mouseReleaseEvent((*reinterpret_cast< std::add_pointer_t<QGraphicsSceneMouseEvent*>>(_a[1]))); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< NB_Box* >(); break;
+            }
+            break;
+        case 1:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< NB_Box* >(); break;
+            }
+            break;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
         int *result = reinterpret_cast<int *>(_a[0]);
         {
-            using _t = void (NB_Box::*)(unsigned int );
+            using _t = void (NB_Box::*)(NB_Box * );
             if (_t _q_method = &NB_Box::NewStatus; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
                 *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (NB_Box::*)(NB_Box * , int , int );
+            if (_t _q_method = &NB_Box::RotateBoxes; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 1;
                 return;
             }
         }
@@ -170,8 +248,8 @@ void *NB_Box::qt_metacast(const char *_clname)
     if (!_clname) return nullptr;
     if (!strcmp(_clname, qt_meta_stringdata_CLASSNB_BoxENDCLASS.stringdata0))
         return static_cast<void*>(this);
-    if (!strcmp(_clname, "QGraphicsRectItem"))
-        return static_cast< QGraphicsRectItem*>(this);
+    if (!strcmp(_clname, "QGraphicsEllipseItem"))
+        return static_cast< QGraphicsEllipseItem*>(this);
     return QObject::qt_metacast(_clname);
 }
 
@@ -181,21 +259,28 @@ int NB_Box::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
-            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 3;
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
     }
     return _id;
 }
 
 // SIGNAL 0
-void NB_Box::NewStatus(unsigned int _t1)
+void NB_Box::NewStatus(NB_Box * _t1)
 {
     void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
     QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void NB_Box::RotateBoxes(NB_Box * _t1, int _t2, int _t3)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t2))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t3))) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
 }
 QT_WARNING_POP
--- docs/moc_nb_game.cpp
+++ docs/moc_nb_game.cpp
@@ -39,35 +39,47 @@ namespace {
 struct qt_meta_stringdata_CLASSNB_GameENDCLASS_t {};
 static constexpr auto qt_meta_stringdata_CLASSNB_GameENDCLASS = QtMocHelpers::stringData(
     "NB_Game",
-    "ForceUpdateGUI",
+    "UpdateBoxColor",
     "",
-    "index",
-    "UpdateBoxColor"
+    "NB_Box*",
+    "ptr_box",
+    "RotateBoxesGUI",
+    "angle",
+    "intensity"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSNB_GameENDCLASS_t {
-    uint offsetsAndSizes[10];
+    uint offsetsAndSizes[16];
     char stringdata0[8];
     char stringdata1[15];
     char stringdata2[1];
-    char stringdata3[6];
-    char stringdata4[15];
+    char stringdata3[8];
+    char stringdata4[8];
+    char stringdata5[15];
+    char stringdata6[6];
+    char stringdata7[10];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSNB_GameENDCLASS_t::offsetsAndSizes) + ofs), len 
 Q_CONSTINIT static const qt_meta_stringdata_CLASSNB_GameENDCLASS_t qt_meta_stringdata_CLASSNB_GameENDCLASS = {
     {
         QT_MOC_LITERAL(0, 7),  // "NB_Game"
-        QT_MOC_LITERAL(8, 14),  // "ForceUpdateGUI"
+        QT_MOC_LITERAL(8, 14),  // "UpdateBoxColor"
         QT_MOC_LITERAL(23, 0),  // ""
-        QT_MOC_LITERAL(24, 5),  // "index"
-        QT_MOC_LITERAL(30, 14)   // "UpdateBoxColor"
+        QT_MOC_LITERAL(24, 7),  // "NB_Box*"
+        QT_MOC_LITERAL(32, 7),  // "ptr_box"
+        QT_MOC_LITERAL(40, 14),  // "RotateBoxesGUI"
+        QT_MOC_LITERAL(55, 5),  // "angle"
+        QT_MOC_LITERAL(61, 9)   // "intensity"
     },
     "NB_Game",
-    "ForceUpdateGUI",
+    "UpdateBoxColor",
     "",
-    "index",
-    "UpdateBoxColor"
+    "NB_Box*",
+    "ptr_box",
+    "RotateBoxesGUI",
+    "angle",
+    "intensity"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -88,11 +100,11 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSNB_GameENDCLASS[] = {
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
        1,    1,   26,    2, 0x0a,    1 /* Public */,
-       4,    1,   29,    2, 0x0a,    3 /* Public */,
+       5,    3,   29,    2, 0x0a,    3 /* Public */,
 
  // slots: parameters
-    QMetaType::Void, QMetaType::UInt,    3,
-    QMetaType::Void, QMetaType::UInt,    3,
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, 0x80000000 | 3, QMetaType::Int, QMetaType::Int,    4,    6,    7,
 
        0        // eod
 };
@@ -106,12 +118,14 @@ Q_CONSTINIT const QMetaObject NB_Game::staticMetaObject = { {
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSNB_GameENDCLASS_t,
         // Q_OBJECT / Q_GADGET
         QtPrivate::TypeAndForceComplete<NB_Game, std::true_type>,
-        // method 'ForceUpdateGUI'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<unsigned int, std::false_type>,
         // method 'UpdateBoxColor'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<unsigned int, std::false_type>
+        QtPrivate::TypeAndForceComplete<NB_Box *, std::false_type>,
+        // method 'RotateBoxesGUI'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<NB_Box *, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>
     >,
     nullptr
 } };
@@ -122,9 +136,27 @@ void NB_Game::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
         auto *_t = static_cast<NB_Game *>(_o);
         (void)_t;
         switch (_id) {
-        case 0: _t->ForceUpdateGUI((*reinterpret_cast< std::add_pointer_t<uint>>(_a[1]))); break;
-        case 1: _t->UpdateBoxColor((*reinterpret_cast< std::add_pointer_t<uint>>(_a[1]))); break;
+        case 0: _t->UpdateBoxColor((*reinterpret_cast< std::add_pointer_t<NB_Box*>>(_a[1]))); break;
+        case 1: _t->RotateBoxesGUI((*reinterpret_cast< std::add_pointer_t<NB_Box*>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[3]))); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< NB_Box* >(); break;
+            }
+            break;
+        case 1:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< NB_Box* >(); break;
+            }
+            break;
         }
     }
 }
@@ -153,7 +185,7 @@ int NB_Game::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
         if (_id < 2)
-            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+            qt_static_metacall(this, _c, _id, _a);
         _id -= 2;
     }
     return _id;
